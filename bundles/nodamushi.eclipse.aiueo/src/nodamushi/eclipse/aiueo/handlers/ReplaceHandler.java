109fd67acba85a5ea0d1c87dedad1cfefbf9cda1
nodamushi <nodamushi@nodamushi>
2017-07-23 05:05:30 +0900

    [change] fragment.e4xmiを使うのを廃止
    よく分からないが、プラグインのインストールをすると、fragmentを使っているとハンドラが認識されたり認識されなかったりする。原因が分からないので廃止。

--- bundles/nodamushi.eclipse.aiueo/src/nodamushi/eclipse/aiueo/handlers/ReplaceHandler.java
+++ bundles/nodamushi.eclipse.aiueo/src/nodamushi/eclipse/aiueo/handlers/ReplaceHandler.java
@@ -2,21 +2,24 @@ package nodamushi.eclipse.aiueo.handlers;
 
 import javax.inject.Named;
 
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.jface.text.ITextOperationTarget;
 import org.eclipse.jface.text.ITextViewer;
 import org.eclipse.swt.custom.StyledText;
-import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.ISources;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 import nodamushi.eclipse.aiueo.internal.IIMEDelegate;
 
-public abstract class ReplaceHandler{
+public abstract class ReplaceHandler extends AbstractHandler{
   private static class Lazy{
     final static IIMEDelegate IMEDELEGATE=IIMEDelegate.findImplements();
   }
@@ -24,6 +27,13 @@ public abstract class ReplaceHandler{
 
   public ReplaceHandler(boolean searchBefore){
     this.searchBefore = searchBefore;
+  }
+  
+  @Override public Object execute(ExecutionEvent event) throws ExecutionException{
+    Shell shell = HandlerUtil.getActiveShell(event);
+    IEditorPart editor = HandlerUtil.getActiveEditor(event);
+    execute(shell, editor);
+    return null;
   }
 
   @Execute
